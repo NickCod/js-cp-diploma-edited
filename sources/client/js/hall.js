dd41a4ea655ffd7d06ecf01affa743f13cffbdd8
Никита Селютин <Garfild.odi@mail.ru>
2023-08-15 21:27:06 +0300

    Доделать

--- sources/client/js/hall.js
+++ sources/client/js/hall.js
@@ -4,3 +4,9 @@ let timeElement = document.querySelector('.buying__info-start');
 if (timeStorage) {
     timeElement.textContent = `Начало сеанса: ${timeStorage}`;
 }
+let nameStorage = localStorage.getItem('name');
+let nameElement = document.querySelector('.buying__info-title');
+
+if(nameStorage){
+    nameElement.textContent = nameStorage;
+}
