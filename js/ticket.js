101ce8efb80e9cd0136ad9a6b73cd1b648a1acc3
Никита Селютин <Garfild.odi@mail.ru>
2023-08-28 01:53:54 +0300

    Qr

--- js/ticket.js
+++ js/ticket.js
@@ -7,32 +7,31 @@ document.addEventListener('DOMContentLoaded', function(){
   
     let ticketName = document.querySelector('.ticket__title');
     let hallId = document.querySelector('.ticket__hall');
-    let ticketId = document.querySelector('.ticket__start');
+    let ticketTime = document.querySelector('.ticket__start');
     let chairs = document.querySelector('.ticket__chairs');
   
     ticketName.textContent = parsedselectedChairs.filmName;
     hallId.textContent = parsedselectedChairs.hallName;
-    ticketId.textContent = parsedselectedChairs.seanceTime;
+    ticketTime.textContent = parsedselectedChairs.seanceTime;
     chairs.textContent = parsedselectedChairs.takenChairs;
   
-    let qr = new QRCode(document.getElementById('qr')); // Создание экземпляра QRCode
+    let qr = new QRCode(document.getElementById('qr')); 
     let savedQRCode = localStorage.getItem('savedQRCode');
   
     if (savedQRCode) {
-      qr.makeCode(savedQRCode); // Отображение сохраненного QR-кода
+      qr.makeCode(savedQRCode); 
     } else {
-      generateAndSaveQRCode(); // Генерация и сохранение нового QR-кода
+      generateAndSaveQRCode(); 
     }
   });
   
   function generateAndSaveQRCode() {
-    let qrContent = generateQRCodeContent(); // Функция для генерации содержимого QR-кода
-    let qr = new QRCode(document.getElementById('qr')); // Создание экземпляра QRCode
-    qr.makeCode(qrContent); // Отображение нового QR-кода
-    localStorage.setItem('savedQRCode', qrContent); // Сохранение нового QR-кода в localStorage
+    let qrContent = generateQRCodeContent(); 
+    let qr = new QRCode(document.getElementById('qr')); 
+    qr.makeCode(qrContent); 
+    localStorage.setItem('savedQRCode', qrContent); 
   }
   function generateQRCodeContent() {
-    // Здесь вы можете сгенерировать содержимое QR-кода на основе нужных данных
-    let qrContent = 'https://vk.com/doggyfaggot'; // Замените на актуальное содержимое
+    let qrContent = JSON.parse(`На фильм: ${parsedselectedChairs.filmName},Зал: ${parsedselectedChairs.hallName},Ряд/Место: ${parsedselectedChairs.takenChairs},Время: ${parsedselectedChairs.ticketTime}`);
     return qrContent;
   }; 
